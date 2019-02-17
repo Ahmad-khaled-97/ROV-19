3ed39e6bc13a3f4b10bba386a39c1936ba0c7837
Ahmed Khaled <ahmed.khaled.abdelaal97@gmail.com>
2019-02-17 22:54:59 +0200

    ESCs&Motors

--- Arduino Code/ControlCode/ControlCode.ino
+++ Arduino Code/ControlCode/ControlCode.ino
@@ -1,17 +1,16 @@
 #include <Servo.h> 
 
 char recv,speed_;
-
-byte bluerobESC1PIN = 2; //Right
-byte bluerobESC2PIN = 3; //Left
-byte bluerobESC3PIN = 4; //Up
-byte bluerobESC4PIN = 5; //Down
-byte bluerobESC5PIN = 6; //Up
-byte bluerobESC6PIN = 7; //Down
-byte bluerobESC7PIN = 8; //Down
 char B;
 int resetPin = 7;
 
+byte bluerobESC1PIN = 5; //HorizontalRightFront_(R1)
+byte bluerobESC2PIN = 6; //HorizontalRightBack_(R2)
+byte bluerobESC3PIN = 7; //HorizontalLeftFront_(L1)
+byte bluerobESC4PIN = 8; //HorizontalLeftBack_(L2)
+byte bluerobESC5PIN = 2; //VerticalRight_(V1)
+byte bluerobESC6PIN = 3; //VerticalLeft_(V2)
+byte bluerobESC7PIN = 4; //VerticalTail_(V3)
 
 Servo bluerobESC1; 
 Servo bluerobESC2; 
@@ -20,6 +19,7 @@ Servo bluerobESC4;
 Servo bluerobESC5; 
 Servo bluerobESC6; 
 Servo bluerobESC7;
+
 
 
 void setup() {
@@ -53,9 +53,12 @@ void setup() {
 pinMode(resetPin, OUTPUT);
 
 }
+ 
+ 
  void loop() {
  
-  if(Serial.available()) {
+  if(Serial.available()) {//Restart
+    
     recv = Serial.read();
     if (recv == 'r'){  
         Serial.println(recv);
@@ -63,18 +66,44 @@ pinMode(resetPin, OUTPUT);
       }
     delay(30);
   }
+  
+  
   if(Serial.available()){
     speed_ = Serial.read();
-      if(recv == 'F'){   //if serial receives FORWARD command, Run those ESCs with these speeds "pwm signals", 1700 is considered medium speed
+      if(recv == 'F'){   //if serial receives FORWARD command
           //Serial.println(recv);
+
+          
+          //Horizontal Motors
           bluerobESC1.writeMicroseconds((1500+(100*(speed_-'0'))));
           bluerobESC2.writeMicroseconds((1500+(100*(speed_-'0'))));
-          bluerobESC3.writeMicroseconds(1500);
-          bluerobESC4.writeMicroseconds(1500);
-          bluerobESC5.writeMicroseconds(1500);
+          bluerobESC3.writeMicroseconds((1500+(100*(speed_-'0'))));
+          bluerobESC4.writeMicroseconds((1500+(100*(speed_-'0'))));
+
+          //Vertical Motors
+          bluerobESC5.writeMicroseconds(1300);
+          bluerobESC6.writeMicroseconds(1300);
           bluerobESC6.writeMicroseconds(1500);
       }
-      else if(recv == 'S'){ //if serial receives STOP command, Run those ESCs with these speeds "pwm signals", 1500 is considered stop signal
+      
+
+      else if (recv == 'B'){ //if serial receives BACKWARD command
+          //Serial.println(recv);
+          
+          //Horizontal Motors
+          bluerobESC1.writeMicroseconds((1500-(100*(speed_-'0'))));
+          bluerobESC2.writeMicroseconds((1500-(100*(speed_-'0'))));
+          bluerobESC3.writeMicroseconds((1500-(100*(speed_-'0'))));
+          bluerobESC4.writeMicroseconds((1500-(100*(speed_-'0'))));
+
+          //Vertical Motors
+          bluerobESC5.writeMicroseconds(1300);
+          bluerobESC6.writeMicroseconds(1300);
+          bluerobESC6.writeMicroseconds(1500);
+      }
+     
+      
+      else if(recv == 'S'){ //if serial receives STOP command
           //Serial.println(recv);
           bluerobESC1.writeMicroseconds(1500);
           bluerobESC2.writeMicroseconds(1500);
@@ -85,40 +114,66 @@ pinMode(resetPin, OUTPUT);
           bluerobESC7.writeMicroseconds(1500);
           
       }
-      else if (recv == 'B'){ //if serial receives BACKWARD command, Run those ESCs with these speeds "pwm signals", 1300 is considered medium speed
-          //Serial.println(recv);
+     
+     
+      else if (recv == 'R'){  //if serial receives RIGHT command
+          
+          //Horizontal Motors
           bluerobESC1.writeMicroseconds((1500-(100*(speed_-'0'))));
           bluerobESC2.writeMicroseconds((1500-(100*(speed_-'0'))));
-          bluerobESC3.writeMicroseconds(1500);
-          bluerobESC4.writeMicroseconds(1500);
-          bluerobESC5.writeMicroseconds(1500);
-          bluerobESC6.writeMicroseconds(1500);
-      }
-      else if (recv == 'R'){  //if serial receives RIGHT command, Run those ESCs with these speeds "pwm signals"
-          bluerobESC1.writeMicroseconds(1500);
-          bluerobESC2.writeMicroseconds((1500+(100*(speed_-'0'))));
-          bluerobESC3.writeMicroseconds(1500);
-          bluerobESC4.writeMicroseconds(1500);
-      }
-      else if (recv == 'L'){  //if serial receives LEFT command, Run those ESCs with these speeds "pwm signals"
-          bluerobESC1.writeMicroseconds((1500+(100*(speed_-'0'))));
-          bluerobESC2.writeMicroseconds(1500);
-          bluerobESC3.writeMicroseconds(1500);
-          bluerobESC4.writeMicroseconds(1500);
-      }
-      else if (recv == 'U'){  //if serial receives UP command
-          bluerobESC1.writeMicroseconds(1500);
-          bluerobESC2.writeMicroseconds(1500);
-          bluerobESC3.writeMicroseconds((1500+(100*(speed_-'0'))));
-          bluerobESC4.writeMicroseconds((1500+(100*(speed_-'0'))));
-          
-      }
-      else if (recv == 'W'){  //if serial receives DOWN command
-          bluerobESC1.writeMicroseconds(1500);
-          bluerobESC2.writeMicroseconds(1500);
           bluerobESC3.writeMicroseconds((1500-(100*(speed_-'0'))));
           bluerobESC4.writeMicroseconds((1500-(100*(speed_-'0'))));
+
+          //Vertical Motors
+          bluerobESC5.writeMicroseconds(1300);
+          bluerobESC6.writeMicroseconds(1300);
+          bluerobESC6.writeMicroseconds(1500);
       }
+     
+      
+      else if (recv == 'L'){  //if serial receives LEFT command
+         //Horizontal Motors
+          bluerobESC1.writeMicroseconds((1500-(100*(speed_-'0'))));
+          bluerobESC2.writeMicroseconds((1500-(100*(speed_-'0'))));
+          bluerobESC3.writeMicroseconds((1500-(100*(speed_-'0'))));
+          bluerobESC4.writeMicroseconds((1500-(100*(speed_-'0'))));
+
+          //Vertical Motors
+          bluerobESC5.writeMicroseconds(1300);
+          bluerobESC6.writeMicroseconds(1300);
+          bluerobESC6.writeMicroseconds(1500);
+      }
+     
+      
+      else if (recv == 'U'){  //if serial receives UP command
+          //Horizontal Motors
+          bluerobESC1.writeMicroseconds(1500);
+          bluerobESC2.writeMicroseconds(1500);
+          bluerobESC3.writeMicroseconds(1500);
+          bluerobESC4.writeMicroseconds(1500);
+
+          //Vertical Motors
+          bluerobESC5.writeMicroseconds((1500+(100*(speed_-'0'))));
+          bluerobESC6.writeMicroseconds((1500+(100*(speed_-'0'))));
+          bluerobESC7.writeMicroseconds((1500+(100*(speed_-'0'))));
+          
+      }
+      
+      
+      else if (recv == 'W'){  //if serial receives DOWN command
+          //Horizontal Motors
+          bluerobESC1.writeMicroseconds(1500);
+          bluerobESC2.writeMicroseconds(1500);
+          bluerobESC3.writeMicroseconds(1500);
+          bluerobESC4.writeMicroseconds(1500);
+
+          //Vertical Motors
+          bluerobESC5.writeMicroseconds((1500-(100*(speed_-'0'))));
+          bluerobESC6.writeMicroseconds((1500-(100*(speed_-'0'))));
+          bluerobESC7.writeMicroseconds((1500-(100*(speed_-'0'))));
+      }
+      
+      
       /*else if (recv == 'T'){  //if serial receives FORWARDRIGHT command
         Serial.println(recv);
       }
@@ -132,18 +187,6 @@ pinMode(resetPin, OUTPUT);
         Serial.println(recv);
       }
       else if (recv == 'g'){  //if serial receives CLOSE GRIPPER command, Run those ESCs with these speeds "pwm signals"
-        Serial.println(recv);
-      }
-      else if (recv == 'A'){  //if serial receives AUTOMODE command, Run those ESCs with these speeds "pwm signals"
-        Serial.println(recv);
-      }
-      else if (recv == 'M'){  //if serial receives RUN MICRO command, Run those ESCs with these speeds "pwm signals"
-        Serial.println(recv);
-      }
-      else if (recv == 'm'){  //if serial receives STOP MICRO command, Run those ESCs with these speeds "pwm signals"
-        Serial.println(recv);
-      }
-      else if (recv == 'P'){  //if serial receives CALCULATE SHAPES command
         Serial.println(recv);
       }
       else if (recv == 'D'){  //if serial receives DETECTED METAL command
